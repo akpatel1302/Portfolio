62b94a3d8d25c583eab48595d34bee02af48a208
Ayush Patel <ayushk0208@gmail.com>
2024-06-24 17:06:36 +0530

    changes

--- js/google_map.js
+++ js/google_map.js
@@ -1,49 +1,210 @@
+// var google;
+
+// function init() {
+//   // Basic options for a simple Google Map
+//   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+//   // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+//   var myLatlng = new google.maps.LatLng(21.1702, 72.8311);
+//   // 39.399872
+//   // -8.224454
+
+//   var mapOptions = {
+//     // How zoomed in you want the map to start at (always required)
+//     zoom: 7,
+
+//     // The latitude and longitude to center the map (always required)
+//     center: myLatlng,
+
+//     // How you would like to style the map.
+//     scrollwheel: false,
+//     styles: [
+//       {
+//         featureType: "administrative.land_parcel",
+//         elementType: "all",
+//         stylers: [{ visibility: "off" }],
+//       },
+//       {
+//         featureType: "landscape.man_made",
+//         elementType: "all",
+//         stylers: [{ visibility: "off" }],
+//       },
+//       {
+//         featureType: "poi",
+//         elementType: "labels",
+//         stylers: [{ visibility: "off" }],
+//       },
+//       {
+//         featureType: "road",
+//         elementType: "labels",
+//         stylers: [{ visibility: "simplified" }, { lightness: 20 }],
+//       },
+//       {
+//         featureType: "road.highway",
+//         elementType: "geometry",
+//         stylers: [{ hue: "#f49935" }],
+//       },
+//       {
+//         featureType: "road.highway",
+//         elementType: "labels",
+//         stylers: [{ visibility: "simplified" }],
+//       },
+//       {
+//         featureType: "road.arterial",
+//         elementType: "geometry",
+//         stylers: [{ hue: "#fad959" }],
+//       },
+//       {
+//         featureType: "road.arterial",
+//         elementType: "labels",
+//         stylers: [{ visibility: "off" }],
+//       },
+//       {
+//         featureType: "road.local",
+//         elementType: "geometry",
+//         stylers: [{ visibility: "simplified" }],
+//       },
+//       {
+//         featureType: "road.local",
+//         elementType: "labels",
+//         stylers: [{ visibility: "simplified" }],
+//       },
+//       {
+//         featureType: "transit",
+//         elementType: "all",
+//         stylers: [{ visibility: "off" }],
+//       },
+//       {
+//         featureType: "water",
+//         elementType: "all",
+//         stylers: [{ hue: "#a1cdfc" }, { saturation: 30 }, { lightness: 49 }],
+//       },
+//     ],
+//   };
+
+//   // Get the HTML DOM element that will contain your map
+//   // We are using a div with id="map" seen below in the <body>
+//   var mapElement = document.getElementById("map");
+
+//   // Create the Google Map using out element and options defined above
+//   var map = new google.maps.Map(mapElement, mapOptions);
+
+//   var addresses = ["Brooklyn"];
+
+//   for (var x = 0; x < addresses.length; x++) {
+//     $.getJSON(
+//       "http://maps.googleapis.com/maps/api/geocode/json?address=" +
+//         addresses[x] +
+//         "&sensor=false",
+//       null,
+//       function (data) {
+//         var p = data.results[0].geometry.location;
+//         var latlng = new google.maps.LatLng(p.lat, p.lng);
+//         new google.maps.Marker({
+//           position: latlng,
+//           map: map,
+//           icon: "images/loc.png",
+//         });
+//       }
+//     );
+//   }
+// }
+// google.maps.event.addDomListener(window, "load", init);
 
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+  // Set the coordinates for Surat, Gujarat
+  var myLatlng = new google.maps.LatLng(21.1702, 72.8311);
 
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
+  var mapOptions = {
+    // Zoom level, higher number is closer
+    zoom: 12,
 
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
-    };
+    // Center the map at Surat, Gujarat
+    center: myLatlng,
 
-    
+    // Disable scroll wheel zoom
+    scrollwheel: false,
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+    // Customize map styles
+    styles: [
+      {
+        featureType: "administrative.land_parcel",
+        elementType: "all",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "landscape.man_made",
+        elementType: "all",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [{ visibility: "simplified" }, { lightness: 20 }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ hue: "#f49935" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels",
+        stylers: [{ visibility: "simplified" }],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [{ hue: "#fad959" }],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "labels",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "road.local",
+        elementType: "geometry",
+        stylers: [{ visibility: "simplified" }],
+      },
+      {
+        featureType: "road.local",
+        elementType: "labels",
+        stylers: [{ visibility: "simplified" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [{ hue: "#a1cdfc" }, { saturation: 30 }, { lightness: 49 }],
+      },
+    ],
+  };
 
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['Brooklyn'];
+  // Get the HTML DOM element that will contain your map
+  var mapElement = document.getElementById("map");
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+  // Create the Google Map using our element and options defined above
+  var map = new google.maps.Map(mapElement, mapOptions);
 
-        });
-    }
-    
+  // Example of adding a marker
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: "Surat, Gujarat",
+    icon: "images/loc.png", // Custom marker icon
+  });
 }
-google.maps.event.addDomListener(window, 'load', init);
+
+// Load the Google Maps API and call the init function
+google.maps.event.addDomListener(window, "load", init);
